d4b3880b65cece64760a98714cf4067d4513f9ff
user <henke.benedikt@gmail.com>
2026-03-10 16:05:05 +0100

    support 1.20.1 and 1.21.1 position format

--- src/main/java/eu/cronmoth/createentityaddon/rendering/tracks/entitymodel/Positions.java
+++ src/main/java/eu/cronmoth/createentityaddon/rendering/tracks/entitymodel/Positions.java
@@ -6,11 +6,29 @@ import java.util.Arrays;
 
 public class Positions {
     @NBTName("Pos") public int[] pos;
+    @NBTName("X") public int x;
+    @NBTName("Y") public int y;
+    @NBTName("Z") public int z;
+
+    public int getX() {
+        return pos!=null ? pos[0] :  x;
+    }
+
+    public int getY() {
+        return pos!=null ? pos[1] :  y;
+    }
+
+    public int getZ() {
+        return pos!=null ? pos[2] :  z;
+    }
 
     @Override
     public String toString() {
         return "Positions{" +
                 "v=" + Arrays.toString(pos) +
+                " || x= " + x +
+                ", y= " + y +
+                ", z= " + z +
                 '}';
     }
 }
